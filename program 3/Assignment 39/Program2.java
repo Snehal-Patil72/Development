93d5686e9ebf103d4650bd39c00a431f7e915ee7
Snehal72 <93419516+Snehal-Patil72@users.noreply.github.com>
2021-12-06 00:41:37 +0530

    Add files via upload

---
+++ program 3/Assignment 39/Program2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+
+class Patern
+{
+
+public void Display(String line)
+{
+
+char Arr[]=line.toCharArray();
+
+for(int i=0;i<Arr.length;i++)
+{
+for(int j=0;j<Arr.length;j++)
+{
+	if(j<Arr.length-i)
+{
+	System.out.printf("%c\t",Arr[j]);
+}
+else
+{
+	System.out.printf("*\t");
+
+}
+}
+System.out.printf("\n");
+}     
+	
+}
+}
+class Program2
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj=new Scanner(System.in);
+
+          String str;
+
+		System.out.println("Enter the string");
+           str=sobj.nextLine();
+		
+
+     Patern pobj=new Patern();
+
+     pobj.Display(str);
+
+	}
+}
