f07b92f137a2770924998ac7fed300479a7ac3dd
Snehal72 <93419516+Snehal-Patil72@users.noreply.github.com>
2022-08-18 13:54:21 +0530

    Add files via upload

---
+++ Angular-Project/MarvellousDemo/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MarvellousDemo';
+  name='snehal dilip ';
+  subject='angular';
+  age=25;
+}
---
+++ Angular-Project/newBinding/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent 
+{
+  title = 'NewBinding';
+
+  public Name:any;
+
+  public str:any;
+
+}
