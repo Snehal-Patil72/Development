ee04112836027c3d23f342b18c5f81ed4315fc59
Snehal72 <93419516+Snehal-Patil72@users.noreply.github.com>
2022-09-25 02:23:01 +0530

    Delete Assignment-13 directory

--- Assignment-13/app/comp-sucess.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CompSucessDirective } from './comp-sucess.directive';
-
-describe('CompSucessDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CompSucessDirective();
-    expect(directive).toBeTruthy();
-  });
-});
